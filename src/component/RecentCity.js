9630c909e0dc58762d8a986e024d34e3d700cc03
Isabella Burin <105743002+isabella010@users.noreply.github.com>
2024-07-31 02:45:29 -0400

    Update RecentCity.js
    removed unused var

--- src/component/RecentCity.js
+++ src/component/RecentCity.js
@@ -1,39 +1,34 @@
-import React, {useState} from 'react'
-import * as ReactBootStrap from "react-bootstrap"
-import { useLocation,useNavigate,useParams } from "react-router-dom";
+import React from 'react';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import './Home.css';
 
-  function withRouter(Component) {
-    function ComponentWithRouterProp(props) {
-      let location = useLocation();
-      let navigate = useNavigate();
-      let params = useParams();
-      return (
-        <Component
-          {...props}
-          router={{ location, navigate, params }}
-        />
-      );
-    }
- 
-    return ComponentWithRouterProp;
-  }
+function RecentCity() {
+    const { city, country, temp, weather, icon } = useParams();
+    const tempFlag = country.toLowerCase();
 
-function RecentCity(props) {
-  var tempFlag = props.router.params.country.toLowerCase();
-  var icon = props.router.params.icon;
-  console.log("props is ", props);
-
-  return (
-    <div className="app">
-      <ReactBootStrap.Card body className="card">
-   <div className="userLocation"> 
-        <h1>{props.router.params.city}, {props.router.params.country} <img src={'http://openweathermap.org/images/flags/'+tempFlag+'.png'}></img></h1>
-        <h2>Temp: {props.router.params.temp} °C </h2>
-        <h2>{props.router.params.weather} <img src={'http://openweathermap.org/img/wn/'+icon+'.png'}></img></h2>
-        </div> 
-   </ReactBootStrap.Card>
-    </div>
-  )
+    return (
+        <div className="app">
+            <div className="card">
+                <div className="userLocation">
+                    <h1>
+                        {city}, {country} 
+                        <img 
+                            src={`http://openweathermap.org/images/flags/${tempFlag}.png`} 
+                            alt={`${country} flag`} 
+                        />
+                    </h1>
+                    <h2>Temp: {temp} °C </h2>
+                    <h2>
+                        {weather} 
+                        <img 
+                            src={`http://openweathermap.org/img/wn/${icon}.png`} 
+                            alt={`Weather icon for ${weather}`} 
+                        />
+                    </h2>
+                </div> 
+            </div>
+        </div>
+    );
 }
-export default withRouter(RecentCity);
+
+export default RecentCity;
