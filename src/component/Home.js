4961f756698666ea64c3c319bbc456d15ea9ad4c
Isabella Burin <105743002+isabella010@users.noreply.github.com>
2024-07-31 03:40:51 -0400

    changed fetch

--- src/component/Home.js
+++ src/component/Home.js
@@ -4,89 +4,73 @@ import axios from "axios";
 import './Home.css';
 
 function Home() {
-    const [latitude, setLatitude] = useState(0);
-    const [longitude, setLongitude] = useState(0);
+    const [latitude, setLatitude] = useState(null);
+    const [longitude, setLongitude] = useState(null);
     const [weather, setWeather] = useState('');
     const [temp, setTemp] = useState(0);
     const [city, setCity] = useState('');
     const [datetime, setDatetime] = useState('');
     const [countryID, setCountryID] = useState('');
     const [icon, setIcon] = useState('');
-    const [showWeather, setShowWeather] = useState(false); // State to manage visibility
-
-    const savePositionToState = (position) => {
-        setLatitude(position.coords.latitude);
-        setLongitude(position.coords.longitude);
-    }
+    const [isDataLoaded, setIsDataLoaded] = useState(false);
 
     const fetchLocalWeather = async () => {
         try {
-            // Ensure geolocation is supported and request permission
-            if (navigator.geolocation) {
-                await new Promise((resolve, reject) => {
-                    navigator.geolocation.getCurrentPosition(
-                        (position) => {
-                            savePositionToState(position);
-                            resolve();
-                        },
-                        (error) => reject(error)
+            await window.navigator.geolocation.getCurrentPosition(
+                async (position) => {
+                    const lat = position.coords.latitude;
+                    const lon = position.coords.longitude;
+
+                    setLatitude(lat);
+                    setLongitude(lon);
+
+                    const res = await axios.get(
+                        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=97eaa9d3202c18880ca8bdc602e4905d`
                     );
-                });
 
-                const res = await axios.get(
-                    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=97eaa9d3202c18880ca8bdc602e4905d`
-                );
-                console.log(res);
-                setTemp(res.data.main.temp);
-                setCity(res.data.name);
-                setIcon(res.data.weather[0].icon);
-                setWeather(res.data.weather[0].main);
-                setCountryID(res.data.sys.country.toLowerCase());
+                    setTemp(res.data.main.temp); 
+                    setCity(res.data.name); 
+                    setIcon(res.data.weather[0].icon);
+                    setWeather(res.data.weather[0].main);
+                    setCountryID(res.data.sys.country.toLowerCase());
 
-                var currentdate = new Date();
-                var tempdate = (currentdate.getMonth() + 1) + "/" + currentdate.getDate() + "/"
-                    + currentdate.getFullYear() + " @ "
-                    + currentdate.getHours() + ":"
-                    + (currentdate.getMinutes() < 10 ? "0" + currentdate.getMinutes() : currentdate.getMinutes());
-
-                setDatetime(tempdate);
-            } else {
-                console.error("Geolocation is not supported by this browser.");
-            }
+                    const currentdate = new Date(); 
+                    const tempdate = `${currentdate.getMonth() + 1}/${currentdate.getDate()}/${currentdate.getFullYear()} @ ${currentdate.getHours()}:${currentdate.getMinutes() < 10 ? '0' + currentdate.getMinutes() : currentdate.getMinutes()}`;
+                    
+                    setDatetime(tempdate);
+                    setIsDataLoaded(true);
+                },
+                (error) => {
+                    console.error("Geolocation error:", error);
+                }
+            );
         } catch (err) {
             console.error(err);
         }
-    }
+    };
 
     return (
         <div className="app">
             <ReactBootStrap.Card body className="card">
-                <div className="userLocation">
-                    <button onClick={() => { 
-                        fetchLocalWeather();
-                        setShowWeather(true); // Show weather info after button click
-                    }}>
-                        Get Weather
-                    </button>
-                    {showWeather && (
-                        <>
-                            <h1>{city}
-                                <img 
-                                    src={`http://openweathermap.org/images/flags/${countryID}.png`} 
-                                    alt={`${countryID} flag`} 
-                                />
-                            </h1>
-                            <h2>{datetime}</h2>
-                            <h2>{temp} °C</h2>
-                            <h2>{weather}
-                                <img 
-                                    src={`http://openweathermap.org/img/wn/${icon}.png`} 
-                                    alt={`Weather icon for ${weather}`} 
-                                />
-                            </h2>
-                        </>
-                    )}
-                </div>
+                <button onClick={fetchLocalWeather}>Get Weather</button>
+                {isDataLoaded && (
+                    <div className="userLocation"> 
+                        <h1>{city} 
+                            <img 
+                                src={`http://openweathermap.org/images/flags/${countryID}.png`} 
+                                alt={`${countryID} flag`} 
+                            />
+                        </h1>
+                        <h2>{datetime}</h2>
+                        <h2>{temp} °C</h2>
+                        <h2>{weather} 
+                            <img 
+                                src={`http://openweathermap.org/img/wn/${icon}.png`} 
+                                alt={`Weather icon for ${weather}`} 
+                            />
+                        </h2> 
+                    </div>
+                )}
             </ReactBootStrap.Card>
         </div>
     );
