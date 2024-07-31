c66d78a01fe1d13bd49a1a7c22044c5af30a3637
Isabella Burin <105743002+isabella010@users.noreply.github.com>
2024-07-31 02:38:53 -0400

    Update Forecast.js
    removed unused imports and variables

--- src/component/ForSearch/Forecast.js
+++ src/component/ForSearch/Forecast.js
@@ -1,29 +1,28 @@
-import React, { useState, Fragment, useEffect } from 'react';
+import React, { useState, Fragment } from 'react';
 import './Forecast.css';
-import PropTypes from 'prop-types'
-import { Container, Row, Col } from 'react-bootstrap';
-import * as ReactBootStrap from "react-bootstrap"
+import PropTypes from 'prop-types';
 import ForecastMain from './ForecastMain';
 import ForecastMore from './ForecastMore';
 import Pagination from './Pagination';
 
-const Forecast = ({forecast, handleForecast}) =>{
+const Forecast = ({ forecast, handleForecast }) => {
     const [editId, setEditId] = useState(null);
     const [currentPage, setCurrentPage] = useState(1);
-    const [postsPerPage, setPostsPerPage] = useState(3);
+    const postsPerPage = 3; // fixed to be a constant
 
-    console.log(forecast)
-    const handleClick = (event, city) =>{
+    console.log(forecast);
+
+    const handleClick = (event, city) => {
         event.preventDefault();
         setEditId(city.id);
         handleForecast(city);
-    } 
+    };
 
     const indexOfLastPost = currentPage * postsPerPage;
-    const indexOfFirstPost = indexOfLastPost - postsPerPage; 
-    const currentPosts = forecast.data.list.slice(indexOfFirstPost, indexOfLastPost)
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = forecast.data.list.slice(indexOfFirstPost, indexOfLastPost);
 
-    //change page
+    // Change page
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     return (
@@ -31,30 +30,31 @@ const Forecast = ({forecast, handleForecast}) =>{
             <div>
                 <table className="mainTable" align="center">
                     <tbody>
-                        {currentPosts.map((city)=>
+                        {currentPosts.map((city) => (
                             <Fragment key={city.id}>
                                 {editId === city.id ? (
                                     <ForecastMore city={city} />
                                 ) : (
-                                    <ForecastMain city={city} handleClick={handleClick}/>
+                                    <ForecastMain city={city} handleClick={handleClick} />
                                 )}
                             </Fragment>
-                        )}
+                        ))}
                     </tbody>
                 </table>
                 <Pagination 
                     postsPerPage={postsPerPage} 
                     totalPosts={forecast.data.list.length} 
-                    paginate={paginate}
+                    paginate={paginate} 
                 />
             </div>
         </>
-      );
+    );
 };
 
 Forecast.propTypes = {
     forecast: PropTypes.shape({
-        data: PropTypes.object,
-    }),
-}
+        data: PropTypes.object.isRequired,
+    }).isRequired,
+};
+
 export default Forecast;
