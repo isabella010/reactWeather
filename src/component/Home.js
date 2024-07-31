892790d1f8f2a7bde2c1cbd412395ecd7be34ebd
Isabella Burin <105743002+isabella010@users.noreply.github.com>
2024-07-31 03:37:34 -0400

    Hide info before button press

--- src/component/Home.js
+++ src/component/Home.js
@@ -4,70 +4,73 @@ import axios from "axios";
 import './Home.css';
 
 function Home() {
-    const [latitude, setLatitude] = useState(null);
-    const [longitude, setLongitude] = useState(null);
+    const [latitude, setLatitude] = useState(0);
+    const [longitude, setLongitude] = useState(0);
     const [weather, setWeather] = useState('');
     const [temp, setTemp] = useState(0);
     const [city, setCity] = useState('');
     const [datetime, setDatetime] = useState('');
     const [countryID, setCountryID] = useState('');
     const [icon, setIcon] = useState('');
-    const [error, setError] = useState('');
-    
-    const fetchWeather = async (lat, lon) => {
-        try {
-            const res = await axios.get(
-                `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=97eaa9d3202c18880ca8bdc602e4905d`
-            );
+    const [showWeather, setShowWeather] = useState(false); // State to manage visibility
 
-            if (res.data && res.data.sys && res.data.sys.country) {
-                setTemp(res.data.main.temp); 
-                setCity(res.data.name); 
+    const savePositionToState = (position) => {
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+    }
+
+    const fetchLocalWeather = async () => {
+        try {
+            // Ensure geolocation is supported and request permission
+            if (navigator.geolocation) {
+                await new Promise((resolve, reject) => {
+                    navigator.geolocation.getCurrentPosition(
+                        (position) => {
+                            savePositionToState(position);
+                            resolve();
+                        },
+                        (error) => reject(error)
+                    );
+                });
+
+                const res = await axios.get(
+                    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=97eaa9d3202c18880ca8bdc602e4905d`
+                );
+                console.log(res);
+                setTemp(res.data.main.temp);
+                setCity(res.data.name);
                 setIcon(res.data.weather[0].icon);
                 setWeather(res.data.weather[0].main);
                 setCountryID(res.data.sys.country.toLowerCase());
-                
-                const currentdate = new Date(); 
-                const tempdate = (currentdate.getMonth() + 1) + "/" + currentdate.getDate() + "/" 
-                    + currentdate.getFullYear() + " @ "  
-                    + currentdate.getHours() + ":"  
+
+                var currentdate = new Date();
+                var tempdate = (currentdate.getMonth() + 1) + "/" + currentdate.getDate() + "/"
+                    + currentdate.getFullYear() + " @ "
+                    + currentdate.getHours() + ":"
                     + (currentdate.getMinutes() < 10 ? "0" + currentdate.getMinutes() : currentdate.getMinutes());
-                
+
                 setDatetime(tempdate);
             } else {
-                console.error('Unexpected API response structure:', res.data);
-                setError('Unable to retrieve country information');
+                console.error("Geolocation is not supported by this browser.");
             }
         } catch (err) {
             console.error(err);
-            setError('Error fetching weather data');
         }
     }
-
-    const handleGeolocation = () => {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                setLatitude(position.coords.latitude);
-                setLongitude(position.coords.longitude);
-                fetchWeather(position.coords.latitude, position.coords.longitude);
-            },
-            (error) => {
-                console.error("Geolocation error:", error);
-                setError('Error retrieving geolocation');
-            }
-        );
-    };
 
     return (
         <div className="app">
             <ReactBootStrap.Card body className="card">
                 <div className="userLocation">
-                    <button onClick={handleGeolocation}>Get Weather</button>
-                    {error ? (
-                        <h2>{error}</h2>
-                    ) : (
+                    <button onClick={() => { 
+                        fetchLocalWeather();
+                        setShowWeather(true); // Show weather info after button click
+                    }}>
+                        Get Weather
+                    </button>
+                    {showWeather && (
                         <>
-                            <h1>{city} 
+                            <h1>{city}
                                 <img 
                                     src={`http://openweathermap.org/images/flags/${countryID}.png`} 
                                     alt={`${countryID} flag`} 
@@ -75,7 +78,7 @@ function Home() {
                             </h1>
                             <h2>{datetime}</h2>
                             <h2>{temp} °C</h2>
-                            <h2>{weather} 
+                            <h2>{weather}
                                 <img 
                                     src={`http://openweathermap.org/img/wn/${icon}.png`} 
                                     alt={`Weather icon for ${weather}`} 
@@ -83,7 +86,7 @@ function Home() {
                             </h2>
                         </>
                     )}
-                </div> 
+                </div>
             </ReactBootStrap.Card>
         </div>
     );
