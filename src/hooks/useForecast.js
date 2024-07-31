d29cab263455efc0ffac81464c4c3714d072d415
Isabella Burin <105743002+isabella010@users.noreply.github.com>
2024-07-31 02:47:19 -0400

    Update useForecast.js

--- src/hooks/useForecast.js
+++ src/hooks/useForecast.js
@@ -1,32 +1,32 @@
 import { useState } from "react";
 import axios from 'axios';
 
-const useForecast = () =>{
-    const [isError, setError] = useState(false);
-    const [isLoading, setLoading] = useState(false);
-    const [forecast, setForcast] = useState(null);
+const useForecast = () => {
+    const [isError, setError] = useState(null);
+    const [forecast, setForecast] = useState(null);
 
     const submitRequest = async location => {
-        const { data } = await axios(`https://api.openweathermap.org/data/2.5/find?callback?&q=` +location + `&units=metric&cnt=50&appid=97eaa9d3202c18880ca8bdc602e4905d`)
-
-        if(!data || data.list.length === 0){
-            setError('There is no location by this name')
-            return;
+        try {
+            const { data } = await axios(`https://api.openweathermap.org/data/2.5/find?q=${location}&units=metric&cnt=50&appid=97eaa9d3202c18880ca8bdc602e4905d`);
+            
+            if (!data || data.list.length === 0) {
+                setError('No location found with this name.');
+                setForecast(null);
+                return;
+            }
+            
+            setError(null);
+            setForecast(data);
+        } catch (error) {
+            setError('An error occurred while fetching the data.');
         }
+    };
 
-        setError(false);
-        setForcast({data});
-    }
-
-    return{
+    return {
         isError, 
-        isLoading, 
         forecast,
         submitRequest,
-    }
-}
+    };
+};
+
 export default useForecast;
-
-
-
-
