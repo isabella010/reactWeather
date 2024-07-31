1951a5f0f15187d658fff06a330c78133608d76b
Isabella Burin <105743002+isabella010@users.noreply.github.com>
2024-07-31 02:43:14 -0400

    Update Pagination.js
    added missing href

--- src/component/ForSearch/Pagination.js
+++ src/component/ForSearch/Pagination.js
@@ -1,24 +1,28 @@
-import React from 'react'
-import './page.css'
+import React from 'react';
+import './page.css';
 
-const Pagination = ({postsPerPage, totalPosts, paginate}) => {
+const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
     const pageNumbers = [];
 
-    for(let i =1; i <= Math.ceil(totalPosts / postsPerPage); i++){
+    for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
         pageNumbers.push(i);
     }
 
-    return(
+    return (
         <nav className="nav">
             {pageNumbers.map(number => (
                 <div key={number} className="page-item">
-                        <a onClick={() => paginate(number)} className="page-link">
-                            {number}
-                        </a>
-                    </div>
-                ))}
+                    <button 
+                        onClick={() => paginate(number)} 
+                        className="page-link" 
+                        aria-label={`Go to page ${number}`}
+                    >
+                        {number}
+                    </button>
+                </div>
+            ))}
         </nav>
-    )
-}
+    );
+};
 
 export default Pagination;
