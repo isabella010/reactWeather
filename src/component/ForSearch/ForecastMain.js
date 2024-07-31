244ad0e8fd00a86a8374a43c35f98e92d554b6b2
Isabella Burin <105743002+isabella010@users.noreply.github.com>
2024-07-31 02:39:47 -0400

    Update ForecastMain.js
    included missing alt atribute

--- src/component/ForSearch/ForecastMain.js
+++ src/component/ForSearch/ForecastMain.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import './Forecast.css';
 
-const ForecastMain = ({city, handleClick}) =>{
-    return(
+const ForecastMain = ({ city, handleClick }) => {
+    return (
         <tr align="center" className="tableRow">
             <td className="tableD">
-            <hr className="horizontal"/>
-            <img src={'http://openweathermap.org/images/flags/'+city.sys.country.toLowerCase()+'.png'}></img> {city.name}, {city.sys.country}  
-            <br/>
-            Current Temperature: {city.main.temp}°C, {city.weather[0].main} <img src={'http://openweathermap.org/img/wn/'+city.weather[0].icon+'@2x.png'}></img>
-            <br/>
-            <button type="button" onClick={(event)=> handleClick(event, city)}>More</button>
+                <hr className="horizontal"/>
+                <img 
+                    src={`http://openweathermap.org/images/flags/${city.sys.country.toLowerCase()}.png`} 
+                    alt={`${city.sys.country} flag`} 
+                /> 
+                {city.name}, {city.sys.country}
+                <br/>
+                Current Temperature: {city.main.temp}°C, {city.weather[0].main} 
+                <img 
+                    src={`http://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`} 
+                    alt={`Weather icon for ${city.weather[0].main}`} 
+                />
+                <br/>
+                <button type="button" onClick={(event) => handleClick(event, city)}>More</button>
                 <hr className="horizontal"/>
             </td>
-            
-            
         </tr>
     );
-}
+};
+
 export default ForecastMain;
