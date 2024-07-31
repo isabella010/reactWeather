f24f31a4f6955c2de412d7c5fdc45b28b6387983
Isabella Burin <105743002+isabella010@users.noreply.github.com>
2024-07-31 02:42:10 -0400

    Update Form.js
    removed unused var

--- src/component/ForSearch/Form.js
+++ src/component/ForSearch/Form.js
@@ -1,40 +1,39 @@
-import PropTypes from 'prop-types'
-import React, {useState} from 'react'
-import * as ReactBootStrap from "react-bootstrap"
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import './Form.css';
 
-const Form = ({submitSearch}) =>{
+const Form = ({ submitSearch }) => {
     const [location, setLocation] = useState('');
 
-    const onSubmit = (e) =>{
+    const onSubmit = (e) => {
         e.preventDefault();
         if (!location || location === '') return;
         submitSearch(location);
-    }
-    
-    return(
-        <>
-        <div className="fakeJumbotron">
-        <form onSubmit={onSubmit}>
-            <input
-                aria-label="location"
-                type="text"
-                placeholder="Enter City Name"
-                required
-                value={location}
-                onChange={e => setLocation(e.target.value)} />
+    };
 
-            <button type="submit" onClick={onSubmit}>
-                SEARCH
-            </button>
-        </form>
-        </div>
+    return (
+        <>
+            <div className="fakeJumbotron">
+                <form onSubmit={onSubmit}>
+                    <input
+                        aria-label="location"
+                        type="text"
+                        placeholder="Enter City Name"
+                        required
+                        value={location}
+                        onChange={e => setLocation(e.target.value)} 
+                    />
+                    <button type="submit">
+                        SEARCH
+                    </button>
+                </form>
+            </div>
         </>
     );
-}
+};
 
 Form.propTypes = {
     submitSearch: PropTypes.func.isRequired,
-}
+};
 
 export default Form;
