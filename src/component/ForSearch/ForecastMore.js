98e26766d35241dfece8b5166885f580d35f8fdb
Isabella Burin <105743002+isabella010@users.noreply.github.com>
2024-07-31 02:41:25 -0400

    Update ForecastMore.js
    fixed unused variables, add an alt attribute to the img elements for accessibility, API Call in component body

--- src/component/ForSearch/ForecastMore.js
+++ src/component/ForSearch/ForecastMore.js
@@ -1,59 +1,60 @@
-import React, { useState } from 'react';
-import axios from "axios"
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 
-const ForecastMore = ({city}) =>{
-    const [sunset, setSunset] = useState(0);
-    const [sunrise, setSunrise] = useState(0);
-    const [finalSet, setFinalSet] = useState(0);
-    const [finalRise, setFinalRise] = useState(0);
+const ForecastMore = ({ city }) => {
+    const [finalSet, setFinalSet] = useState('');
+    const [finalRise, setFinalRise] = useState('');
 
-    var currentdate = new Date(); 
-    var tempdate = (currentdate.getMonth()+1) + "/" + currentdate.getDate()  + "/" + currentdate.getFullYear() + " @ "  + currentdate.getHours() + ":"  
-    if(currentdate.getMinutes() < 10){
-        tempdate+= "0" + currentdate.getMinutes()
-    }else{
-        tempdate+= currentdate.getMinutes()
-    }
-    
-    const fetchLocalWeather = async () => {
-        try{
-          const res = await axios.get(
-            `https://api.openweathermap.org/data/2.5/weather?id=` + city.id + `&units=metric` + `&appid=97eaa9d3202c18880ca8bdc602e4905d`
-          );
-          console.log(res);
-          setSunset(res.data.sys.sunset);
-          setSunrise(res.data.sys.sunrise);
-          var rise = (sunrise*1000);
-          let rdate = new Date(rise);
-          setFinalRise(rdate.toLocaleTimeString())
-          var set = (sunset*1000);
-          let sdate = new Date(set);
-          setFinalSet(sdate.toLocaleTimeString())
+    var currentdate = new Date();
+    var tempdate = (currentdate.getMonth() + 1) + "/" + currentdate.getDate() + "/" + currentdate.getFullYear() + " @ " + currentdate.getHours() + ":";
+    tempdate += currentdate.getMinutes() < 10 ? "0" + currentdate.getMinutes() : currentdate.getMinutes();
 
-        }catch(err){
-          console.error(err);
-        }
-      }
-      fetchLocalWeather();
+    useEffect(() => {
+        const fetchLocalWeather = async () => {
+            try {
+                const res = await axios.get(
+                    `https://api.openweathermap.org/data/2.5/weather?id=${city.id}&units=metric&appid=97eaa9d3202c18880ca8bdc602e4905d`
+                );
+                console.log(res);
+                const sunrise = res.data.sys.sunrise * 1000;
+                const sunset = res.data.sys.sunset * 1000;
+                setFinalRise(new Date(sunrise).toLocaleTimeString());
+                setFinalSet(new Date(sunset).toLocaleTimeString());
+            } catch (err) {
+                console.error(err);
+            }
+        };
 
-    return(
+        fetchLocalWeather();
+    }, [city.id]); // Dependency array to fetch weather data when city.id changes
+
+    return (
         <tr align="center" className="tableRow">
             <td className="tableD">
-            <hr className="horizontal"/>
-            <img src={'http://openweathermap.org/images/flags/'+city.sys.country.toLowerCase()+'.png'}></img> {city.name}, {city.sys.country}  
-            <br/>
-            Current Temperature: {city.main.temp}°C, {city.weather[0].main} <img src={'http://openweathermap.org/img/wn/'+city.weather[0].icon+'@2x.png'}></img>
-            <hr/>
-            Temp min: {city.main.temp_min}, Temp max: {city.main.temp_max}, Wind speed: {city.wind.speed}m/s
-            <br/>
-            Humidity: {city.main.humidity}%, Pressure: {city.main.pressure}hpa
-            <br/> 
-            Sunrise: {finalRise} Sunset: {finalSet}
-            <br/>
-            Last Updated: {tempdate}
-            <hr className="horizontal"/>
+                <hr className="horizontal"/>
+                <img 
+                    src={`http://openweathermap.org/images/flags/${city.sys.country.toLowerCase()}.png`} 
+                    alt={`${city.sys.country} flag`} 
+                /> 
+                {city.name}, {city.sys.country}
+                <br/>
+                Current Temperature: {city.main.temp}°C, {city.weather[0].main} 
+                <img 
+                    src={`http://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`} 
+                    alt={`Weather icon for ${city.weather[0].main}`} 
+                />
+                <hr/>
+                Temp min: {city.main.temp_min}, Temp max: {city.main.temp_max}, Wind speed: {city.wind.speed}m/s
+                <br/>
+                Humidity: {city.main.humidity}%, Pressure: {city.main.pressure}hpa
+                <br/> 
+                Sunrise: {finalRise} Sunset: {finalSet}
+                <br/>
+                Last Updated: {tempdate}
+                <hr className="horizontal"/>
             </td>
         </tr>
     );
-}
+};
+
 export default ForecastMore;
